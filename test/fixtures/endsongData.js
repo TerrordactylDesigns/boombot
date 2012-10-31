f5d2f1858bf6368bd0e103dd233bfefde8da6dfa
Michael Belardo <mbelardo1@gmail.com>
2012-10-30 23:48:13 -0400

    MOAR TESTS

---
+++ test/fixtures/endsongData.js
@@ -0,0 +1,43 @@
+module.exports = {
+   "command": "endsong",
+   "room": {
+      "name": "@alain_gilbert",
+      "created": 1315459578.63,
+      "shortcut": "alaingilbert",
+      "name_lower": "@alain_gilbert",
+      "roomid": "4e6851fa14169c2255108b8c",
+      "metadata": {
+         "dj_full": false,
+         "djs": ["4deadb0f4fe7d013dc0555f1"],
+         "upvotes": 0,
+         "privacy": "public",
+         "max_djs": 5,
+         "downvotes": 0,
+         "random": 0.400335315393,
+         "userid": "4deadb0f4fe7d013dc0555f1",
+         "listeners": 2,
+         "djcount": 1,
+         "max_size": 200,
+         "djthreshold": 0,
+         "moderator_id": ["4deadb0f4fe7d013dc0555f1"],
+         "current_song": {
+            "djid": "4deadb0f4fe7d013dc0555f1",
+            "_id": "4ddd08ad845daf4c250035cc",
+            "djname": "@alain_gilbert",
+            "starttime": 1327229746.2,
+            "metadata": {
+               "album": "The Fame",
+               "artist": "Lady GaGa",
+               "coverart": "http://images.mndigital.com/albums/018/334/899/m.jpeg",
+               "song": "Poker Face",
+               "length": 237,
+               "mnid": "18334907",
+               "genre": "Pop"
+            }
+         },
+         "current_dj": "4deadb0f4fe7d013dc0555f1",
+         "votelog": [['50661f0390001b4', 'up' ],[ '4df0cbe44fe7d2', 'up']]
+      }
+   },
+   "success": true
+}
--- test/index.js
+++ test/index.js
@@ -1,35 +1,45 @@
 /*
   Variables
 */
-var test = require('tap').test;
-// var robot = require('../lib/robot');
-var events = require('../lib/events');
+var blankBoombot
+  , deregisteredData = require('./fixtures/deregisteredData')
+  , endsongData = require('./fixtures/endsongData')
+  , events = require('../lib/events')
+  , newsongData = require('./fixtures/newsongData')
+  , registeredData = require('./fixtures/registeredData')
+  , roomChangedData = require('./fixtures/roomChangedData')
+  , snaggedData = null
+  , test = require('tap').test;
 // im lazy
 Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i--) {
-        if (this[i] == obj) {
-            return true;
-        }
-    }
-    return false;
+  var i = this.length
+  while (i--) {
+    if (this[i] == obj)
+      return true
+  }
+  return false
 }
-var blankBoombot = function() {
-    this.config = {consolelog: false};
-    this.theUsersList = {};
-    this.shutUp = true;
-    this.snagCounter = 0;
-    this.DJMode = false;
-    this.djQueue = [];
-    this.queue = false;
-    this.yank = false;
-    this.queueLength = 3;
-    this.autoNod = false;
-    this.nextUp = {};
-    this.blackList = [];
+blankBoombot = function() {
+  this.autoNod = false
+  this.blackList = []
+  this.bot = {}
+  this.config = {consolelog: false}
+  this.DJMode = false
+  this.djQueue = []
+  this.nextUp = {}
+  this.queue = false
+  this.queueLength = 3
+  this.RemoveFromQueue = null
+  this.shutUp = true
+  this.snagCounter = 0
+  this.theUsersList = {}
+  this.yank = false
 }
 /*
   TESTS
+*/
+/*
+  BOOMBOT.js TESTS
 */
 //verify we can parse a copy of the example.config.json file
 //test('test loading configuration file', function(t){
@@ -38,68 +48,151 @@ var blankBoombot = function() {
   // t.end();
 //})
 /*
-  EVENTS.JS TESTS
+  EVENTS.js TESTS
 */
 // roomChangedEvent tests
-var roomChangedData = require('./fixtures/roomChangedData');
 // verify theUsersList is reset
 test('test userList reset', function(t){
-  var boombot = new blankBoombot();
-  boombot.theUsersList['test'] = { name:'unittest', userid:'a1234' };
-  events.roomChangedEvent(boombot, roomChangedData);
-  t.notOk(boombot.theUsersList['test'], 'Users list is successfully reset on roomChange');
-  t.end();
+  var boombot = new blankBoombot()
+  boombot.theUsersList['test'] = { name:'unittest', userid:'a1234' }
+  events.roomChangedEvent(boombot, roomChangedData)
+  t.notOk(boombot.theUsersList['test'], 'Users list is successfully reset on roomChange')
+  t.end()
 })
 // verify theUsersList is filled
 test('test that the theUsersList is filled on roomChange', function(t) {
-  var boombot = new blankBoombot();
-  events.roomChangedEvent(boombot, roomChangedData);
-  t.ok(boombot.theUsersList['4f4ce636a3f7512f70000fef'], 'theUsersList is successfully filled on roomChange');
-  t.end();
+  var boombot = new blankBoombot()
+  events.roomChangedEvent(boombot, roomChangedData)
+  t.ok(boombot.theUsersList['4f4ce636a3f7512f70000fef'], 'theUsersList is successfully filled on roomChange')
+  t.end()
 })
 // registeredEvent tests
-var registeredData = require('./fixtures/registeredData');
 // verify the user is added to theUsersList
 test('test that the user is added to theUsersList', function(t) {
-  var boombot = new blankBoombot();
-  events.registeredEvent(boombot, registeredData);
-  t.ok(boombot.theUsersList[registeredData.user[0].userid], 'User was successfully added to theUsersList on room entry');
-  t.end();
+  var boombot = new blankBoombot()
+  events.registeredEvent(boombot, registeredData)
+  t.ok(boombot.theUsersList[registeredData.user[0].userid], 'User was successfully added to theUsersList on room entry')
+  t.end()
+})
+// verify banned users are booted
+test('test that a blacklisted user is booted on registeredEvent', function(t) {
+  var actual = null
+    , boombot = new blankBoombot();
+  boombot.bot = {
+    bootUser : function(userid, message) {
+      actual = userid
+    }
+  }
+  boombot.blackList = [{user: {userid: '4f569e32a3f751581a009f39'}}]
+  events.registeredEvent(boombot, registeredData)
+  t.equal(actual, '4f569e32a3f751581a009f39', 'the blacklisted user was booted on registeredEvent')
+  t.end()
 })
 // TODO - tests for announcers
 // deregisteredEvent tests
-var deregisteredData = require('./fixtures/deregisteredData');
 // verify the user is removed from theUsersList
 test('test that the user is removed from theUsersList', function(t) {
-  var boombot = new blankBoombot();
-  events.deregisteredEvent(boombot, registeredData);
-  t.notOk(boombot.theUsersList[deregisteredData.user[0].userid], 'User was successfully removed from theUsersList on leaving room');
-  t.end();
+  var boombot = new blankBoombot()
+  events.deregisteredEvent(boombot, deregisteredData)
+  t.notOk(boombot.theUsersList[deregisteredData.user[0].userid], 'User was successfully removed from theUsersList on leaving room')
+  t.end()
 })
-// TODO - tests for queue removal
+// tests for queue removal
+test('test that the user is removed from the queue on deregisteredEvent', function(t) {
+  var actual = null
+    , boombot = new blankBoombot();
+  boombot.djQueue = ['1234', '4321', '50073697eb35c17ea7000039']
+  boombot.RemoveFromQueue = function(id, name) {
+    actual = id
+  }
+  events.deregisteredEvent(boombot, deregisteredData)
+  t.equal(actual, '50073697eb35c17ea7000039', 'User was successfully removed from the queue on deregisteredEvent')
+  t.end()
+})
 // TODO - update_votes tests
 // newsongEvent tests
-var newsongData = require('./fixtures/newsongData');
 // verify the snagCounter is reset
-test('test that the snagCounter is reset', function(t) {
-  var boombot = new blankBoombot();
-  boombot.snagCounter = 2;
-  boombot.theUsersList[newsongData.room.metadata.current_dj] = {name: 'test', IncPlays: function(){}};
-  events.newsongEvent(boombot, newsongData);
-  t.notOk(boombot.snagCounter == 2, 'The snagCounter was successfully reset on newsong');
-  t.equal(0, boombot.snagCounter, 'The snagCounter was successfully reset on newsong');
-  t.end();
+test('test newsongEvent resets the snagCounter', function(t) {
+  var boombot = new blankBoombot()
+  boombot.snagCounter = 2
+  boombot.theUsersList[newsongData.room.metadata.current_dj] = {name: 'test', IncPlays: function(){}}
+  events.newsongEvent(boombot, newsongData)
+  t.notOk(boombot.snagCounter == 2, 'The snagCounter was successfully reset on newsong')
+  t.equal(0, boombot.snagCounter, 'The snagCounter was successfully reset on newsong')
+  t.end()
+})
+// verify the current Djs play count increments
+test('test that the current Dj\'s play count increments on newsongEvent', function(t) {
+  var boombot = new blankBoombot()
+    , testDj = require('../models/dj');
+  boombot.theUsersList['4f4ce636a3f7512f70000fef'] = new testDj('@GodOfThisAge', '4f4ce636a3f7512f70000fef')
+  events.newsongEvent(boombot, newsongData)
+  t.equal(boombot.theUsersList['4f4ce636a3f7512f70000fef'].plays, 1, 'current Dj play count is incremented on newsongEvent')
+  t.end()
 })
 // TODO - test for queue control, autoNod
-// TODO - endsongEvent tests
-//var endsongData = require('./fixtures/endsongData');
 // snaggedEvent tests
-var snaggedData = null;
 // verify the snagCounter increments
-test('test that the snagCounter increments', function(t) {
-  var boombot = new blankBoombot();
-  events.snaggedEvent(boombot, snaggedData);
-  t.notOk(boombot.snagCounter == 0, 'The snagCounter did not remain 0 on snag event');
-  t.equal(1, boombot.snagCounter, 'The snagCounter was incremented on snag event');
-  t.end();
+test('test snaggedEvent increments snagCounter', function(t) {
+  var boombot = new blankBoombot()
+  events.snaggedEvent(boombot, snaggedData)
+  t.notOk(boombot.snagCounter == 0, 'The snagCounter did not remain 0 on snag event')
+  t.equal(1, boombot.snagCounter, 'The snagCounter was incremented on snag event')
+  t.end()
 })
+// endsongEvent tests
+// verify stats are spoken
+test('test that the after song stats are parsed and spoken on endsongEvent', function(t) {
+  var actual = null
+    , boombot = new blankBoombot();
+  boombot.bot = {
+    speak: function(text) {
+      actual = text
+    }
+  }
+  boombot.config['stats'] = true
+  boombot.shutUp = false
+  boombot.snagCounter = 3
+  events.endsongEvent(boombot, endsongData)
+  t.equal(actual, "Poker Face by Lady GaGa got :+1: 0 :-1: 0 <3 3", 'The vote log is parsed and spoke on endsongEvent')
+  t.end()
+})
+// test for queue removals if valid
+test('test that the Dj is removed if their play count is higher than the q max', function(t) {
+  var actual = false
+    , boombot = new blankBoombot()
+    , testDj = require('../models/dj');
+  boombot.bot = {
+    remDj : function(x) {
+      actual = true
+    }
+  }
+  boombot.queue = true
+  boombot.queueLength = 1
+  boombot.theUsersList['4deadb0f4fe7d013dc0555f1'] = new testDj('@GodOfThisAge', '4f4ce636a3f7512f70000fef')
+  boombot.theUsersList['4deadb0f4fe7d013dc0555f1'].plays = 2
+  events.endsongEvent(boombot, endsongData)
+  t.equal(actual, true, 'User is removed from the stage if play count is higher than the q max')
+  t.end()
+})
+// test to make sure you are not removed if the q limit is not met
+test('test that the Dj is not removed if their play count is less than the q max', function(t) {
+  var actual = false
+    , boombot = new blankBoombot()
+    , testDj = require('../models/dj');
+  boombot.bot = {
+    remDj : function(x) {
+      actual = true
+    }
+  }
+  boombot.queue = true
+  boombot.queueLength = 3
+  boombot.theUsersList['4deadb0f4fe7d013dc0555f1'] = new testDj('@GodOfThisAge', '4f4ce636a3f7512f70000fef')
+  boombot.theUsersList['4deadb0f4fe7d013dc0555f1'].plays = 2
+  events.endsongEvent(boombot, endsongData)
+  t.equal(actual, false, 'User is left on stage if the play count is less than the q max')
+  t.end()
+})
+// TODO - tests for add_djEvent
+// TODO - tests for rem_djEvent
+// TODO - tests for handleCommand
