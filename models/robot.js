c45b5527410f0caf4241f48cfe5f14d920abe851
Michael Belardo <mbelardo1@gmail.com>
2012-11-04 09:10:57 -0500

    force queue to announce if a space open on join. if multiple spaces are open and the first person hops up, announce second, etc.

--- models/robot.js
+++ models/robot.js
@@ -56,8 +56,10 @@ Robot.prototype.RemoveFromQueue = function(userID, userName, private) {
   var DJIndex = this.djQueue.indexOf(userID);
   if (DJIndex != -1) {
     this.djQueue.splice(DJIndex, 1);
-    //this.bot.speak('fine ' + userName + " don't join our party....");
     this.respond(userID, 'fine ' + userName + " don't join our party....", private);
+    if (DJIndex == 0) {
+      this.runQueue();
+    }
   } else {
     this.respond(userID, "You're not in the queue.... type q+ to add yourself.", private);
   }
@@ -69,22 +71,40 @@ Robot.prototype.RemoveFromQueue = function(userID, userName, private) {
 /**///
 /**/// Returns
 /**/// return - Timer controlled announcement and enforcement of Queue
-Robot.prototype.runQueue = function(currDjs) {
-  if (this.djQueue.length > 0 && currDjs.length < 5) {
-    // store the users name and id to stop the recursion insanity i had going on
-    this.nextUp = {"name" : this.theUsersList[this.djQueue[0]].name, "id" : this.djQueue[0]};
-    //this.bot.speak('@' + this.nextUp.name + ' you have 30 seconds starting now to step up');
-    this.respond(this.djQueue, '@' + this.nextUp.name + ' you have 30 seconds starting now to step up', true);
-    this.respond(this.djQueue, '@' + this.nextUp.name + ' you have 30 seconds starting now to step up', false);
-    var thisBot = this;
-    setTimeout(function(){
-      // after 30 seconds if that user isnt on stage drop them from queue, hear the lamentation of their women, and run again.
-      if (thisBot.djQueue.length > 0 && thisBot.djQueue[0] == thisBot.nextUp.id) {
-        thisBot.djQueue.splice(0,1);
-        thisBot.runQueue(currDjs);
-      }
-    }, 30000);
-  }
+Robot.prototype.runQueue = function() {
+  var self = this;
+  this.bot.roomInfo(false, function(data){
+    var currDjs = data.room.metadata.djs;
+    if (self.djQueue.length > 0 && currDjs.length < 5) {
+      // store the users name and id to stop the recursion insanity i had going on
+      self.nextUp = {"name" : self.theUsersList[self.djQueue[0]].name, "id" : self.djQueue[0]};
+      self.respond(self.djQueue, '@' + self.nextUp.name + ' you have 30 seconds starting now to step up', true);
+      self.respond(self.djQueue, '@' + self.nextUp.name + ' you have 30 seconds starting now to step up', false);
+      var thisBot = self;
+      setTimeout(function(){
+        // after 30 seconds if that user isnt on stage drop them from queue, hear the lamentation of their women, and run again.
+        if (thisBot.djQueue.length > 0 && thisBot.djQueue[0] == thisBot.nextUp.id) {
+          thisBot.djQueue.splice(0,1);
+          thisBot.runQueue(currDjs);
+        }
+      }, 30000);
+    }
+    });
+    // if (this.djQueue.length > 0 && currDjs.length < 5) {
+    //   // store the users name and id to stop the recursion insanity i had going on
+    //   this.nextUp = {"name" : this.theUsersList[this.djQueue[0]].name, "id" : this.djQueue[0]};
+    //   this.respond(this.djQueue, '@' + this.nextUp.name + ' you have 30 seconds starting now to step up', true);
+    //   this.respond(this.djQueue, '@' + this.nextUp.name + ' you have 30 seconds starting now to step up', false);
+    //   var thisBot = this;
+    //   setTimeout(function(){
+    //     // after 30 seconds if that user isnt on stage drop them from queue, hear the lamentation of their women, and run again.
+    //     if (thisBot.djQueue.length > 0 && thisBot.djQueue[0] == thisBot.nextUp.id) {
+    //       thisBot.djQueue.splice(0,1);
+    //       thisBot.runQueue(currDjs);
+    //     }
+    //   }, 30000);
+    // }
+
 };
 
 module.exports = Robot;
