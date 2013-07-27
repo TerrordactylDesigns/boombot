9f9a6a2639ebf442d103fc7fc846b8257d7aeff6
TerrordactylDesigns <mbelardo1@gmail.com>
2013-07-26 20:06:03 -0400

    fix tests for updates

--- test/eventstests.js
+++ test/eventstests.js
@@ -86,6 +86,11 @@ test('test newsongEvent resets the snagCounter', function(t) {
   var boombot = new blankBoombot()
   boombot.snagCounter = 2
   boombot.theUsersList[newsongData.room.metadata.current_dj] = {name: 'test', IncPlays: function(){}}
+  boombot.bot = {
+    getFans: function(a,b) {
+      return 0
+    }
+  }
   events.newsongEvent(boombot, newsongData)
   t.notOk(boombot.snagCounter == 2, 'The snagCounter was successfully reset on newsong')
   t.equal(0, boombot.snagCounter, 'The snagCounter was successfully reset on newsong')
@@ -96,6 +101,11 @@ test('test that the current Dj\'s play count increments on newsongEvent', functi
   var boombot = new blankBoombot()
     , testDj  = require('../models/dj')
   boombot.theUsersList['4f4ce636a3f7512f70000fef'] = new testDj('@GodOfThisAge', '4f4ce636a3f7512f70000fef')
+  boombot.bot = {
+    getFans: function(a,b) {
+      return 0
+    }
+  }
   events.newsongEvent(boombot, newsongData)
   t.equal(boombot.theUsersList['4f4ce636a3f7512f70000fef'].plays, 1, 'current Dj play count is incremented on newsongEvent')
   t.end()
@@ -119,16 +129,21 @@ test('test snaggedEvent increments snagCounter', function(t) {
 test('test that the after song stats are parsed and spoken on endsongEvent', function(t) {
   var actual  = null
     , boombot = new blankBoombot()
+    , testDj  = require('../models/dj')
+  boombot.theUsersList['4f4ce636a3f7512f70000fef'] = new testDj('@GodOfThisAge', '4f4ce636a3f7512f70000fef')
   boombot.bot = {
     speak: function(text) {
       actual = text
+    },
+    getFans: function(a,b) {
+      return boombot.bot.speak(endsongData.room.metadata.current_song.metadata.song + " by " + endsongData.room.metadata.current_song.metadata.artist + " got :+1: " + endsongData.room.metadata.upvotes + " :-1: " +  endsongData.room.metadata.downvotes + " <3 " + boombot.snagCounter + " :star: " + (0 - boombot.theUsersList['4f4ce636a3f7512f70000fef'].startFans))
     }
   }
   boombot.config['stats'] = true
   boombot.shutUp = false
   boombot.snagCounter = 3
   events.endsongEvent(boombot, endsongData)
-  t.equal(actual, "Poker Face by Lady GaGa got :+1: 0 :-1: 0 <3 3", 'The vote log is parsed and spoke on endsongEvent')
+  t.equal(actual, "Poker Face by Lady GaGa got :+1: 0 :-1: 0 <3 3 :star: 0", 'The vote log is parsed and spoke on endsongEvent')
   t.end()
 })
 // test for queue removals if valid
